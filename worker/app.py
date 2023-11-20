a8f5385290c32ba8ce7ec302a70de234e5aba041
Alvaro Salazar <ah.salazar@uniandes.edu.co>
2023-11-19 21:22:59 -0500

    (fix) worker service updated

--- worker/app.py
+++ worker/app.py
@@ -1,7 +1,11 @@
+from google.cloud import pubsub_v1
 from celery import Celery
 from sqlalchemy import create_engine, MetaData, Table, update
 import os
 import ffmpeg
+from google.cloud import storage
+import json
+import ast
 
 # Configuración de Celery
 BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
@@ -14,16 +18,70 @@ metadata = MetaData()
 metadata.bind = engine
 task_table = Table('tasks', metadata, autoload_with=engine)
 
+
+# Configuración de Pub/Sub
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path("estudio-gcp-301920", "video_converter-sub")
+
+def callback(message):
+    print(f"Recibido mensaje: {message}")
+    print(f"mensaje Data: {message.data}")
+    try:
+        # Decodificar de bytes a string
+        message_str = message.data.decode("utf-8")
+        task_data = ast.literal_eval(message_str)
+        # # Si ya es un diccionario, úsalo directamente
+        # if isinstance(message_str, dict):
+
+        # # Intenta decodificar como JSON si los datos son un string
+        # elif isinstance(message_str, str):
+        #     task_data = json.loads(message_str)
+        # else:
+        #     raise ValueError("Formato de mensaje no reconocido")
+
+        process_task_from_queue.delay(task_data)
+        message.ack()
+
+    except (json.JSONDecodeError, ValueError) as e:
+        print(f"Error al procesar el mensaje: {e}")
+
+
+# Escucha de mensajes
+subscriber.subscribe(subscription_path, callback=callback)
+
+
 def convert_file_format(storedFileName, newFormat):
 
-    file_name, _ = os.path.splitext(storedFileName)
-    input_file = os.path.join(os.environ.get('SAVE_PATH', '/file_conversor/uploaded/'), storedFileName)
+    bucket_name = os.environ.get('GCP_BUCKET_NAME', 'miso-4204-feog-exp1')
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
 
-    output_file = os.path.join(os.environ.get('CONVERT_PATH', '/file_conversor/processed/'), file_name +'.'+newFormat )
+    # Ruta del archivo original en el bucket
+    file_path = f"uploaded/{storedFileName}"
+    blob_input = bucket.blob(file_path)
+    input_file = f"/tmp/{storedFileName}"
+
+    # Descargar el archivo a un directorio temporal
+    blob_input.download_to_filename(input_file)
+
+    file_name, _ = os.path.splitext(storedFileName)
+    output_file_name = f"{file_name}.{newFormat}"
+    output_file = f"/tmp/{output_file_name}"
+    # input_file = os.path.join(os.environ.get('SAVE_PATH', '/file_conversor/uploaded/'), storedFileName)
+    # output_file = os.path.join(os.environ.get('CONVERT_PATH', '/file_conversor/processed/'), file_name +'.'+newFormat )
 
     stream = ffmpeg.input(input_file)
     stream = ffmpeg.output(stream, output_file)
+
     ffmpeg.run(stream)
+
+    destination_blob_name = f"processed/{file_name}.{newFormat}"
+    blob = bucket.blob(destination_blob_name)
+
+    with open(output_file, 'rb') as file_obj:
+        blob.upload_from_file(file_obj)
+
+
     return output_file
 
 @celery_app.task(name='process_task_from_queue')
